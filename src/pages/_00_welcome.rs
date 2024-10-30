e8ada254763921422471fd58716f825b2b053ff7
madonuko <madonuko@outlook.com>
2024-10-31 01:37:25 +0800

    fix(pages/welcome): make skip config button better position

--- src/pages/_00_welcome.rs
+++ src/pages/_00_welcome.rs
@@ -35,25 +35,27 @@ crate::generate_page!(Welcome {
 
     gtk::Box {
         set_valign: gtk::Align::End,
-        set_halign: gtk::Align::Center,
+        set_halign: gtk::Align::Fill,
         set_hexpand: true,
         set_orientation: gtk::Orientation::Horizontal,
 
         libhelium::Button {
-            set_valign: gtk::Align::End,
+            set_is_textual: true,
             set_halign: gtk::Align::Start,
             set_label: &gettext("Skip Configuration"),
             connect_clicked => Self::Input::SkipConfig,
         },
 
+        gtk::Box { set_halign: gtk::Align::Fill, set_hexpand: true },
+
         libhelium::Button {
             set_is_pill: true,
-            set_valign: gtk::Align::End,
-            set_halign: gtk::Align::Center,
+            set_halign: gtk::Align::End,
             set_label: &gettext("Let's Go"),
             inline_css: "padding-left: 48px; padding-right: 48px",
             add_css_class: "suggested-action",
             connect_clicked => Self::Input::Nav(NavAction::Next),
         },
+
     }
 );
