125edee5d90589b8633ed564a09e3e1ac1639dc4
madonuko <madonuko@outlook.com>
2025-01-17 11:29:47 +0800

    feat(taidan_catalogue_parser): better cli output

--- taidan_catalogue_parser/main.rs
+++ taidan_catalogue_parser/main.rs
@@ -1,12 +1,16 @@
-fn main() {
+fn main() -> color_eyre::Result<()> {
     color_eyre::install().unwrap();
     let mut args = std::env::args();
     let bin = args.next().unwrap();
     if args.len() == 0 {
         eprintln!("Usage: {bin} <path/to/yaml> <path/to/another/yaml> ...");
-        return;
+        std::process::exit(1);
     }
     for p in args {
-        taidan_catalogue_parser::Category::parse_path(std::path::Path::new(&p)).unwrap();
+        print!(" Check: {p}\r");
+        taidan_catalogue_parser::Category::parse_path(std::path::Path::new(&p))
+            .map_err(|e| color_eyre::Report::msg(format!("cannot parse {p}")).wrap_err(e))?;
+        println!(" PASS : {p}");
     }
+    Ok(())
 }
