755b25eca37f2433c6e99f879239593b2c33da8b
madonuko <madonuko@outlook.com>
2025-03-04 01:38:12 +0800

    fix(systemd): working service and now preset that autoenables service

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,5 +7,6 @@ done
 
 install -Dpm644 data/sysusers.d/taidan.conf             -t $DESTDIR/usr/lib/sysusers.d/
 install -Dpm644 data/systemd/*.service                  -t $DESTDIR/usr/lib/systemd/system/
+install -Dpm644 data/systemd/*.preset                   -t $DESTDIR/usr/lib/systemd/system-preset/
 install -Dpm644 data/polkit-1/rules.d/100-taidan.rules  -t $DESTDIR/usr/share/polkit-1/rules.d/
 install -Dpm644 data/pam.d/taidan -t $DESTDIR/etc/pam.d/
