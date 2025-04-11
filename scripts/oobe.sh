cfb5321e01972c12ad7fbc8425e526615ffa1627
lleyton@fyralabs.com <lleyton@fyralabs.com>
2025-04-10 21:21:00 -0700

    fix: comment out oobe langpacks

--- scripts/oobe.sh
+++ scripts/oobe.sh
@@ -9,16 +9,16 @@ lang=`. /etc/locale.conf; echo ${LANG%.*}`
 wait
 if echo $langlist | grep $lang; then
   # stdout are packages that taidan will install
-  echo "pkg: ultramarine-langpacks-$lang"
+  # echo "pkg: ultramarine-langpacks-$lang"
 elif echo $langlist | grep ${lang%_*}; then
-  echo "pkg: ultramarine-langpacks-${lang%_*}"
+  # echo "pkg: ultramarine-langpacks-${lang%_*}"
 fi
 
 
 # add video group
 if grep -E '^video:' /etc/group; then
   # $1 is path to json file for `settings`
-  usermod -aG video `jq -r '.username' $1` 
+  usermod -aG video `jq -r '.username' $1`
 fi
 
 hostnamectl hostname ultramarine
